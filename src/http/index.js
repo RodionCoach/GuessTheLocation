719f9092f926461bffcb268cc4a75afc1c91dd1d
Rodion <rodion.kochetkov@effective-soft.com>
2020-05-22 12:35:15 +0300

    remove http module

--- src/http/index.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const baseURL = process.env.REACT_APP_BASE_API;
-
-export const http = axios.create({
-  baseURL,
-});
--- src/http/mock.js
+++
@@ -1,5 +0,0 @@
-import MockAdapter from 'axios-mock-adapter';
-
-import { http } from './index';
-
-export const mock = new MockAdapter(http, { delayResponse: 3000 });
